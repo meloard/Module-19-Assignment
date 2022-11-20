8e0ca067e8380d7e5b5827f1d4cab15550157e80
Josh Adam <joshsabag@gmail.com>
2022-11-20 17:56:51 -0500

    updated code

---
+++ client/src/js/database.js
@@ -0,0 +1,39 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+
+    const contactDb = await openDB('jate', 1);
+    const tx = contactDb.transaction('jate', 'READ-WRITE');
+    const store = tx.objectStore('jate');
+    const request = store.put({ id: 1, value: content });
+    const result = await request;
+}
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+    
+    const contactDb = await openDB('jate', 1);
+    const tx = contactDb.transaction('jate', 'READ-ONLY');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    return result?.value;
+  };
+    
+    
+    
+
+initdb();
